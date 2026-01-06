b3016df711ebe61e373713e03d972e2a7a68bfbb
MaksSvid <sviderskiymaks04@gmail.com>
2026-01-06 13:31:46 +0000

    menu backend

--- app/handlers.py
+++ app/handlers.py
@@ -2,13 +2,21 @@ from aiogram import Bot, Dispatcher, F, Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart, Command
 
+import app.keyboards as keyboard
+import logging
+
 # All handlers should be attached to the Router (or Dispatcher)
 router = Router()
 
 # Command handlers
 @router.message(CommandStart())
 async def cmd_start(message: Message) -> None:
-    await message.answer("Hello! I'm Mirror NPC.")
+
+    user_id = message.from_user.id
+    logging.info(f"user_id={user_id}")
+
+    await message.answer("Hello! I'm Mirror NPC.🤖", 
+                         reply_markup=keyboard.main)
 
 @router.message(Command('help'))
 async def cmd_help(message: Message) -> None:
@@ -17,3 +25,8 @@ async def cmd_help(message: Message) -> None:
 @router.message(F.text == 'Окей')
 async def cmd_reply(message: Message) -> None:
     await message.answer("Ага okay okay поговори")
+
+# Gemini menu options
+@router.message(F.text == 'GeminiAI')
+async def gemini_menu(message: Message) -> None:
+    await message.answer('Testing options keyboard', reply_markup=keyboard.gemini_options)
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,3 +1,12 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton 
+from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton, 
+                           InlineKeyboardMarkup, InlineKeyboardButton)  
 
-main = ReplyKeyboardMarkup()
+main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='GeminiAI')],
+                                    [KeyboardButton(text='Job Reports')],
+                                    [KeyboardButton(text='Crypto Alerts')],
+                                    [KeyboardButton(text='Settings')]],
+                                    resize_keyboard=True,
+                                    input_field_placeholder='Choose the options...')
+
+gemini_options = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Gemini Chat', callback_data='gemini_chat')],                                             
+                                                ])
